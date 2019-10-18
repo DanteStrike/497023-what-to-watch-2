c85748df548b3a3e9ccf2cca2813bd3e5640f450
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-18 21:48:48 +0300

    CREATE Catalog component

---
+++ src/components/catalog/catalog.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import MovieCard from "../movie-card/movie-card.jsx";
+
+const Catalog = (props) => {
+
+  return (
+    <section className="catalog">
+      <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+      <ul className="catalog__genres-list">
+        <li className="catalog__genres-item catalog__genres-item--active">
+          <a href="#" className="catalog__genres-link">All genres</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Comedies</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Crime</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Documentary</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Dramas</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Horror</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Kids & Family</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Romance</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Sci-Fi</a>
+        </li>
+        <li className="catalog__genres-item">
+          <a href="#" className="catalog__genres-link">Thrillers</a>
+        </li>
+      </ul>
+
+      <div className="catalog__movies-list">
+      </div>
+
+      <div className="catalog__more">
+        <button className="catalog__button" type="button">Show more</button>
+      </div>
+    </section>
+  );
+};
+
+
+export default Catalog;
