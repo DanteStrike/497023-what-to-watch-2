387ea88934adf6f29bbafd688a603e775ffa7efe
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-26 22:04:27 +0300

    CREATE MovieBackground component

---
+++ src/components/movie-background/movie-background.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import MovieBackground from "./movie-background.jsx";
+
+it(`Render correctly MovieBackground component`, () => {
+  const component = renderer
+    .create(
+        <MovieBackground/>
+    ).toJSON();
+
+  expect(component).toMatchSnapshot();
+});
