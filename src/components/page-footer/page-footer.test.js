d4f80c8be77c8363518de0a9c209b1f0c916aa33
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-26 22:07:56 +0300

    REFACTOR tests

--- src/components/page-footer/page-footer.test.js
+++ src/components/page-footer/page-footer.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import PageFooter from "./page-footer.jsx";
 
-it(`render correctly PageFooter component`, () => {
+it(`Render correctly PageFooter component`, () => {
   const PageFooterComponent = renderer
     .create(
         <PageFooter/>
