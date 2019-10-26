69f34018009991535e64750ee89726d73b046082
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-26 22:07:26 +0300

    REFACTOR MoviePreview component based on new components

--- src/components/movie-preview/movie-preview.jsx
+++ src/components/movie-preview/movie-preview.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PageHeader from "../page-header/page-header.jsx";
+import MovieBackground from "../movie-background/movie-background.jsx";
+import MoviePoster from "../movie-poster/movie-poster.jsx";
+import MovieControlPanel from "../movie-control-panel/movie-control-panel.jsx";
 
 const MoviePreview = () => {
   return (
     <section className="movie-card">
-      <div className="movie-card__bg">
-        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
-      </div>
+      <MovieBackground/>
 
       <h1 className="visually-hidden">WTW</h1>
 
@@ -14,32 +15,12 @@ const MoviePreview = () => {
 
       <div className="movie-card__wrap">
         <div className="movie-card__info">
-          <div className="movie-card__poster">
-            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
-          </div>
 
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">Drama</span>
-              <span className="movie-card__year">2014</span>
-            </p>
+          <MoviePoster
+            isBig={false}
+          />
 
-            <div className="movie-card__buttons">
-              <button className="btn btn--play movie-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list movie-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"></use>
-                </svg>
-                <span>My list</span>
-              </button>
-            </div>
-          </div>
+          <MovieControlPanel/>
         </div>
       </div>
     </section>
