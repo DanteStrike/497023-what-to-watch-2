97099c0e9ac8532d163104451dc26848c40fac01
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-26 15:30:22 +0300

    CREATE films mock

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import {films} from "./mocks/films.js";
 
 const init = () => {
-  const moviesList = [
-    {
-      title: `Fantastic Beasts: The Crimes of Grindelwald`,
-      image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
-    },
-    {
-      title: `Johnny English`,
-      image: `img/johnny-english.jpg`
-    },
-    {
-      title: `Shutter Island`,
-      image: `img/shutter-island.jpg`
-    }
-  ];
-
   ReactDOM.render(
       <App
-        moviesList = {moviesList}
+        films={films}
       />,
       document.querySelector(`#root`)
   );
