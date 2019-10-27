e92d8311eff05d08abdfe65bd003c9286099eaac
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-27 21:34:35 +0300

    COMBINE tests

--- src/components/movie-card/movie-card.e2e.test.js
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -5,36 +5,38 @@ import MovieCard from "./movie-card.jsx";
 
 Enzyme.configure({adapter: new Adapter()});
 
-it(`should callback to parent component film ID on mouse over`, () => {
-  const onFilmMouseHover = jest.fn();
-  const component = shallow(
-      <MovieCard
-        id={2}
-        title={``}
-        image={``}
-        titleLinkHref={`#`}
-        onFilmMouseHover={onFilmMouseHover}
-        onFilmMouseLeave={jest.fn()}
-      />
-  );
+describe(`Callbacks to parent`, () => {
+  it(`should callback to parent component film ID on mouse over`, () => {
+    const onFilmMouseHover = jest.fn();
+    const component = shallow(
+        <MovieCard
+          id={2}
+          title={``}
+          image={``}
+          titleLinkHref={`#`}
+          onFilmMouseHover={onFilmMouseHover}
+          onFilmMouseLeave={jest.fn()}
+        />
+    );
 
-  component.simulate(`mouseenter`);
-  expect(onFilmMouseHover).toBeCalledWith(2);
-});
+    component.simulate(`mouseenter`);
+    expect(onFilmMouseHover).toBeCalledWith(2);
+  });
 
-it(`should callback to parent component on mouse out`, () => {
-  const onFilmMouseLeave = jest.fn();
-  const component = shallow(
-      <MovieCard
-        id={2}
-        title={``}
-        image={``}
-        titleLinkHref={`#`}
-        onFilmMouseHover={jest.fn()}
-        onFilmMouseLeave={onFilmMouseLeave}
-      />
-  );
+  it(`should callback to parent component on mouse out`, () => {
+    const onFilmMouseLeave = jest.fn();
+    const component = shallow(
+        <MovieCard
+          id={2}
+          title={``}
+          image={``}
+          titleLinkHref={`#`}
+          onFilmMouseHover={jest.fn()}
+          onFilmMouseLeave={onFilmMouseLeave}
+        />
+    );
 
-  component.simulate(`mouseleave`);
-  expect(onFilmMouseLeave).toBeCalled();
+    component.simulate(`mouseleave`);
+    expect(onFilmMouseLeave).toBeCalled();
+  });
 });
