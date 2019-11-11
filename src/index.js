fba30705de8aaeade16fd01de68eff83be18b54f
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-11 20:55:39 +0300

    CONNECT redux devtools extension

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import {createStore} from "redux";
 import {reducer} from "./reducer/reducer.js";
 
 const init = (films) => {
-  const store = createStore(reducer);
+  const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  );
 
   ReactDOM.render(
       <Provider store={store}>
