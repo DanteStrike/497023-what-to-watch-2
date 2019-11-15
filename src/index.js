ed166443e2daddd0f6b854d15ea7d31df0a8c0be
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-16 01:41:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app/app.jsx";
-import {films as filmsMock} from "./mocks/films.js";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import {reducer} from "./reducers/reducer.js";
 
-const init = (films) => {
+import {applyMiddleware, createStore, combineReducers} from "redux";
+import {Provider} from "react-redux";
+
+import thunk from "redux-thunk";
+import {compose} from "recompose";
+
+import configureAPI from "./server/configure-API.js";
+
+import {filmsReducer} from "./reducers/films/index.js";
+import {genreFilterReducer} from "./reducers/genre-filter";
+
+import App from "./components/app/app.jsx";
+import setupApp from "./reducers/app/loader";
+
+
+const init = () => {
+  const api = configureAPI((...args) => store.dispatch(...args));
+
+  const rootReducer = combineReducers({
+    films: filmsReducer,
+    genreFilter: genreFilterReducer
+  });
+
   const store = createStore(
-      reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      rootReducer,
+      compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      )
   );
+
+  store.dispatch(setupApp());
 
   ReactDOM.render(
       <Provider store={store}>
-        <App
-          films={films}
-        />
+        <App/>
       </Provider>,
       document.querySelector(`#root`)
   );
 };
 
-init(filmsMock);
+init();
 
