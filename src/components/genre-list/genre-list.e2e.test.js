3146de255787db0d73006dc478f239ea7520a00e
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-16 15:12:43 +0300

    Update genre-list.e2e.test.js

--- src/components/genre-list/genre-list.e2e.test.js
+++ src/components/genre-list/genre-list.e2e.test.js
@@ -6,5 +6,21 @@ import {GenreList} from "./genre-list.jsx";
 Enzyme.configure({adapter: new Adapter()});
 
 describe(`GenreList should work correctly`, () => {
+  it(``, () => {
+    const preventDefault = jest.fn();
+    const onGenreChange = jest.fn();
+    const component = shallow(
+        <GenreList
+          currentFilter={`All genre`}
+          genres={[`All genre`, `others`]}
+          filterGenre={`All genre`}
+          onGenreChange={onGenreChange}
+        />
+    );
 
+    component.find(`.catalog__genres-item`).at(1).find(`.catalog__genres-link`).simulate(`click`, {preventDefault});
+    expect(preventDefault).toBeCalledTimes(1);
+    expect(onGenreChange).toBeCalledTimes(1);
+    expect(onGenreChange).toHaveBeenNthCalledWith(1, `others`);
+  });
 });
