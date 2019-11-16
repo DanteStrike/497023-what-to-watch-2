5ee39d4ab797862723b4e97948e1e3011473bef0
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-16 14:13:04 +0300

    REMOVE MoviePage and simple Router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,9 @@
 import React from "react";
 import MainPage from "../main-page/main-page.jsx";
-// import MoviePage from "../movie-page/movie-page.jsx";
-
-const getPage = () => {
-  switch (location.pathname) {
-    case `/`:
-    case `/main`:
-      return (
-        <MainPage/>
-      );
-    // case `/details`:
-    //   return (
-    //     <MoviePage/>
-    //   );
-  }
-
-  return null;
-};
 
 const App = () => {
   return (
-    getPage()
+    <MainPage/>
   );
 };
 
