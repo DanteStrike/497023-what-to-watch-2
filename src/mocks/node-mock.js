c962c6fe58228480761b99018aaad1932b9ee040
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-16 14:50:52 +0300

    Create node-mock.js

---
+++ src/mocks/node-mock.js
@@ -0,0 +1,10 @@
+export const createNodeMock = (element) => {
+  if (element.type === `video`) {
+    return {
+      src: null,
+      isMuted: null,
+      poster: null
+    };
+  }
+  return null;
+};
