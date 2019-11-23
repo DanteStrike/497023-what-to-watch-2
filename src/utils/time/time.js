806c07a149258336c66f5fb5f5963539a75759a4
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-23 14:53:45 +0300

    Update time.js

--- src/utils/time/time.js
+++ src/utils/time/time.js
@@ -1,4 +1,11 @@
 export const Time = {
   MILLISECONDS_IN_SECOND: 1000,
-  MILLISECONDS_IN_MINUTE: 60 * 1000
+  MINUTES_IN_HOUR: 60
+};
+
+export const formatTimeToHM = (time) => {
+  const hours = Math.floor(time / Time.MINUTES_IN_HOUR);
+  const minutes = time - hours * Time.MINUTES_IN_HOUR;
+
+  return `${hours}h ${minutes}m`;
 };
