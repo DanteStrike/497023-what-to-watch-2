db383542cddcf880337939eb6cd6d531830bd04d
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-12-07 15:52:47 +0300

    Update page-header.jsx

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -4,11 +4,11 @@ import Logo from "../logo/logo.jsx";
 
 
 const PageHeader = (props) => {
-  const {mixinClass, rightPart} = props;
+  const {mixinClass = ``, rightPart} = props;
 
   return (
     <header className={`page-header ${mixinClass}`}>
-      <Logo/>
+      <Logo isLight={false}/>
       {rightPart}
     </header>
   );
