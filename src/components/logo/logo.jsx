dc3bb1450a5d2a479b81123503176da779b476c6
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-12-07 15:51:32 +0300

    FIX logo className

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -9,7 +9,7 @@ const Logo = (props) => {
 
   return (
     <div className="logo">
-      <Link to="/" className={`logo__link ${(isLight) ? `logo__link--light` : ``}`}>
+      <Link to="/" className={`logo__link${(isLight) ? ` logo__link--light` : ``}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
